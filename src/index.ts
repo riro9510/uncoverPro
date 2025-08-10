ab5a1b857b3408d98b7377b99558ae1feb90ea66
riro9510 <112458155+riro9510@users.noreply.github.com>
2025-08-09 20:07:47 -0600

    2025-08-09  feature: change in port to production  Jira/Trello:  ReadyToRelease: false

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import { connectDB } from './config/database.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 10000;
 
 connectDB();
 app.use(express.json());
@@ -12,8 +12,7 @@ app.use('/api', router, (req, res) => {
   console.log( 'Response:', res.statusCode, res.statusMessage,res);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 export default app;
