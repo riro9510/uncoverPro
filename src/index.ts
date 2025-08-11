3888186b5522dc8f7e1ed83e090910415d641f09
riro9510 <112458155+riro9510@users.noreply.github.com>
2025-08-11 07:39:10 -0600

    2025-08-11  feature: front  Jira/Trello:  ReadyToRelease: false

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import express from 'express';
 import router from './routes/index.routes.js';
 import 'dotenv/config';
 import { connectDB } from './config/database.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 10000;
+const frontendPath = path.join(__dirname, '../front');
 
 connectDB();
 app.use(express.json());
-app.use('/api', router, (req, res) => {
-  console.log('Response:', res.statusCode, res.statusMessage, res);
+app.use(express.static(frontendPath));
+app.use('/api', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
