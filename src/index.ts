52c8b59509645ac8fa4d43f24ac1d0ed0f085bb5
riro9510 <112458155+riro9510@users.noreply.github.com>
2025-08-11 09:05:43 -0600

    2025-08-11  fix: visual fix Jira/Trello:  ReadyToRelease: false

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,25 @@ import router from './routes/index.routes.js';
 import 'dotenv/config';
 import { connectDB } from './config/database.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
+//import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 10000;
-const frontendPath = path.join(__dirname, '../front');
+//const frontendPath = path.join(__dirname, '../front');
 
 connectDB();
 app.use(express.json());
-app.use(express.static(frontendPath));
+//app.use(express.static(frontendPath));
 app.use('/api', router, (req, res) => {
   console.log('Response:', res.statusCode, res.statusMessage, res);
 });
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
-});
+});*/
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
