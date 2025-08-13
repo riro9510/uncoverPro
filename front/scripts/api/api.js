049f5ba1dd764cd4279f26033d8eb180935e5e75
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-08-12 22:04:43 -0600

    2025-08-12  feature: Front is capable of run through the whole json, and switch between languages Jira/Trello:  ReadyToRelease: false

---
+++ front/scripts/api/api.js
@@ -0,0 +1,46 @@
+
+
+const env = "dev";
+const baseURL = (env === 'production' 
+  ? 'https://uncoverpro.onrender.com/api' 
+  : 'http://localhost:3000/api');
+
+const getToken = () => localStorage.getItem('api_token');
+
+const setToken = (token) => localStorage.setItem('api_token', token);
+
+const getAuthHeaders = () => {
+  const token = getToken();
+  return token ? { Authorization: 'Bearer ' + token } : {};
+};
+
+export const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    ...getAuthHeaders(),
+  },
+  withCredentials: true,
+});
+
+
+api.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    ...getAuthHeaders(),
+  };
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    if (response.data && response.data.token) {
+      setToken(response.data.token);
+    }
+    return response;
+  },
+  (error) => {
+    console.error('API Error:', error);
+    return Promise.reject(error);
+  }
+);
